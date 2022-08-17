f4172bc3b9798f8c1182ef35f54ed68ba5a66a2f
oscaros <komuntuoscar@gmail.com>
2022-08-17 19:09:19 +0300

    my first commit

---
+++ assignment.py
@@ -0,0 +1,7 @@
+#vars
+my_name = "Oscar Komuntu"
+age = 30
+weight = 78.0
+
+#print
+print ("My name is " + my_name + ", Iam " + str(age) + " years old. I weigh " + str(weight) + " kgs")
---
+++ lesson_one.py
@@ -0,0 +1,16 @@
+print('hello world')
+
+#vars
+name = "oscar"
+
+#string
+name ="oscar"
+#numbers
+no = 2
+nos = 1,2,3,4,5,6
+#Float
+flt = 1.5
+#bool
+isMale = True
+#None
+age = None
