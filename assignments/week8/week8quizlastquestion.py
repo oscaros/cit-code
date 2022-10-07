29847270e63a6244d66deff309c261b365f5e98b
oscaros <komuntuoscar@gmail.com>
2022-10-07 20:06:53 +0300

    week8quiz solved last question

--- assignments/week8/week8quizlastquestion.py
+++ assignments/week8/week8quizlastquestion.py
@@ -1,9 +1,43 @@
 import os
-import sys
 
-# def CreateFile():
-try:
-    with open(os.getcwd()+'answers.txt', 'w') as file:
-        file.write('Create a new text file!')
-except FileNotFoundError:
-    print("The 'docs' directory does not exist")
+def CreateFile():
+    try:
+        open(os.getcwd() +'/answers2.txt', "w")
+    except FileNotFoundError:
+        print("The file does not exist")
+
+def WriteAnswers(answers: dict):
+    try:
+        with open(os.getcwd() +'/answers2.txt', 'w') as file:
+            file.write("Answers to week8 quiz questions \n")
+            for key, value in answers.items():
+                file.write(f"Qn. {key} : Answer is {value} \n")
+
+    except FileNotFoundError:
+        print("The file does not exist")
+
+def ReadFile():
+    try:
+        with open(os.getcwd() +'/answers2.txt', 'r+') as file:
+            mytext = file.readlines()
+            for i in mytext:
+                print(i)
+    except FileNotFoundError:
+        print("The file does not exist")
+
+def main():
+    CreateFile()
+    answers = {
+        "1": "d",
+        "2": "c",
+    }
+    WriteAnswers(answers)
+    ReadFile()
+
+if __name__ == "__main__":
+    main()
+
+        
+        
+
+
